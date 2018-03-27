8e6ff6f051d2cf85d561261e802d5ff6f4e58070
Steph Japs <stephjaps@gmail.com>
2018-03-27 16:58:39 -0500

    trying to figure this out

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,56 @@
 
+//  Refer to in class exercise to figure out how to start game by hitting the key. Rock paper scissors game. 
+
+//  Refer to demo in class that so if you hear the key and he shows up on the screen. 
+
+//  For number of gases, anytime a key is pressed after the game starts the number of gases decrements by i—.  As opposed to i++ that we have been using in class so far. 
+
+///////////////////////////////////
+
+//  Write the code easiest to hardest. First, do the thing with the game starts when you press a key. Second, do the thing where number of gases decrements every time a key is hit. 
+
+///////////////////////////////////
+
+
+
+
+//  Key is pressed to start game 
+
+//  this can be changed to an array if more answers are needed 
+//  other answers might be teacher, books, classroom, pencils, paper, eraser
+var  answer = "chalkboard";
+
+//  create array from letters in answer 
+// https://stackoverflow.com/questions/6484670/how-do-i-split-a-string-into-an-array-of-characters
+var correctLetters;
+
+
+//  initially set to 12 but will be decremented with each guess (each  time the key is hit after the start of the game ) 
+var numberOfGuesses = 12;
+
+
+
+//  originally set to zero but will be incremented with every win 
+var wins;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////
+
+
 // create one word that is going to be guessed. 
     // figure out later if we need to do more.
 
