74da1b834f6fc32a4b181c1b24d643f0925f7934
Steph Japs <stephjaps@gmail.com>
2018-03-27 09:52:08 -0500

    basic styling and layout plus some psuedo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+
+// create one word that is going to be guessed. 
+    // figure out later if we need to do more.
+
+// number of guesses allowed is 12
+
+// usr presses key to start game
+
+
+
+// user hits key to guess
+    // "Number of Guesses Remaining" decreases by 1
+    // script checks to see if letter is in word
+        // if letter is NOT in word, letter displays under "Letters Already Guessed"
+        // if letter IS in word, it appears in the appropriate place/places under "Current Word"
+
+// game ends when word is guessed or no guesses remain
+    // WIN if word is guessed, 
+        // number under "Wins" increases by 1
+        // word appears above 
+    // LOSS (video doesn't say) Next game starts??????
